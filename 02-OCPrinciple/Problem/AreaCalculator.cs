ab18717b7283ba8d40bac593ad6dcf86d3a2ec1d
Jose Manuel Ganchozo <jganchozo@outlook.com>
2023-05-08 18:17:11 -0400

    Open Close Principle

---
+++ 02-OCPrinciple/Problem/AreaCalculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem
+{
+    internal class AreaCalculator
+    {
+        public string Area(List<object> shapes)
+        {
+            string area = string.Empty;
+
+            foreach (var shape in shapes)
+            {
+                if (shape is Rectangle)
+                {
+                    Rectangle rectangle = (Rectangle)shape;
+                    area += "Rectangulo: " + rectangle.With * rectangle.Height;
+                }
+                else
+                {
+                    Circle circle = (Circle)shape;
+                    area += "Circulo: " + circle.Radius * circle.Radius * Math.PI;
+                }
+
+                area += Environment.NewLine;
+            }
+
+            return area;
+        }
+    }
+}
